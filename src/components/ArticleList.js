7915f948a5e0d2f3ab1ceddb48df6ccdbb8e911e
Gail Hobbs <gbhobbs97@gmail.com>
2023-02-26 20:18:26 -0800

    new bug fix

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,14 +1,20 @@
 import Article from "./Article"
 
-function ArticleList({post}) {
-    const posts = post.map((article, index) => {
-        const elementWithDate = <Article key={index} title={article.title} date={article.date} preview={article.preview} minutes={article.minutes}/>;
-        const elementWithoutDate = <Article key={index} title={article.title} review={article.preview} minutes={article.minutes}/>;
-        return (article.date ? elementWithDate : elementWithoutDate)
-    });
+function ArticleList({posts}) {
+    let allPosts = posts;
 
+
+    if (posts) {
+        allPosts = posts.map((article) => {
+            const elementWithDate = <Article key={article.id} title={article.title} date={article.date} preview={article.preview} minutes={article.minutes}/>;
+            const elementWithoutDate = <Article key={article.id} title={article.title} preview={article.preview} minutes={article.minutes}/>;
+            return (article.date ? elementWithDate : elementWithoutDate)
+        });
+    }
+
+    
     return (<main>
-        {posts}
+        {posts ? allPosts : posts}
     </main>);
 
 }
